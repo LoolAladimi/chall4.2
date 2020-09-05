d3b3aac71f6cad948ab08552082d5ea0d81c217a
lool-aladimi <loolaladimi@gmail.com>
2020-09-06 02:22:26 +0300

    Initial commit

---
+++ src/chall4.2.kt
@@ -0,0 +1,5 @@
+fun main() {
+    var loolList= listOf("lool","Ahmed","saad","mohmemd")
+    println("the first name is" +"  "+ loolList.first())
+    println("the last name is" +"  "+ loolList.last())
+}
